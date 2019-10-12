a544efbfcd014e43d9e359edc35c21ea79e538d3
elviredev <sandrine.rodriguez@hotmail.com>
2019-10-12 19:09:23 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './services/authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shop-service-frontend';
+
+  constructor(private authService: AuthenticationService){}
+
+  isAdmin(){
+    return this.authService.isAdmin();
+  }
+
+  isUser(){
+    return this.authService.isUser();
+  }
+
+  isAuthenticated(){
+    return this.authService.isAuthenticated();
+  }
 }
