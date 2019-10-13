ee518aef3f29924420a0c724aebbeb63665b35c5
elviredev <sandrine.rodriguez@hotmail.com>
2019-10-13 19:47:43 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,34 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { ApplisComponent } from './applis/applis.component';
 import { ContentsComponent } from './contents/contents.component';
 import { LoginComponent } from './login/login.component';
+import { AdminApplisComponent } from './admin/admin-applis/admin-applis.component';
+import { AdminContentsComponent } from './admin/admin-contents/admin-contents.component';
+import { AdminUsersComponent } from './admin/admin-users/admin-users.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ApplisComponent,
     ContentsComponent,
-    LoginComponent
+    LoginComponent,
+    AdminApplisComponent,
+    AdminContentsComponent,
+    AdminUsersComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
