e7f372c71a0ef9c10aeba76a82de95ed3f4350b1
elviredev <sandrine.rodriguez@hotmail.com>
2019-10-13 19:49:05 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContentsComponent } from './contents/contents.component';
 import { LoginComponent } from './login/login.component';
+import { AdminApplisComponent } from './admin/admin-applis/admin-applis.component';
+import { AdminContentsComponent } from './admin/admin-contents/admin-contents.component';
+import { AdminUsersComponent } from './admin/admin-users/admin-users.component';
+import { ContactComponent } from './contact/contact.component';
+import { ApplisComponent } from './applis/applis.component';
 
 const routes: Routes = [
+  { path: 'applis', component: ApplisComponent },
+  { path: 'contents', component: ContentsComponent },
   { path: 'contents/:urlContents', component: ContentsComponent },
-  { path: 'login', component: LoginComponent }
+  //{ path: 'contents/:p1', component: ContentsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'adminApplis', component: AdminApplisComponent },
+  { path: 'adminContents', component: AdminContentsComponent },
+  { path: 'adminUsers', component: AdminUsersComponent },
+  { path: 'contact', component: ContactComponent }
 ];
 
 @NgModule({
